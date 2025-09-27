9f8c4c0e0ef88d2e9dc55b79551e23d6ccfec73d
Lau <laurafernandat5@gmail.com>
2025-09-26 20:18:57 -0400

    c

--- lms-i-s/app.py
+++ lms-i-s/app.py
@@ -2,9 +2,20 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+# Función para la página principal (index.html)
 @app.route('/')
-def home():
+def index():  # <-- ¡Este es el 'index' que usas en url_for('index')!
     return render_template('index.html')
+
+# Función para la página de servicios (servicios.html)
+@app.route('/servicios')
+def servicios(): # <-- ¡Este es el 'servicios' que usas en url_for('servicios')!
+    return render_template('servicios.html')
+    
+# Función para la página de contacto (contacto.html)
+@app.route('/contacto')
+def contacto(): # <-- ¡Este es el 'contacto' que usas en url_for('contacto')!
+    return render_template('contacto.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
